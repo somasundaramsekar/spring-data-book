9222efb4ddda7d5bb0bb3511eb92beedaa7ac2d1
Somasundaram Sekar <somasundaram@outlook.com>
2016-04-25 17:45:52 +0530

    Configed

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -1,14 +1,38 @@
 package com.example;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-import com.example.domain.QCustomer;
+
+import com.querydsl.sql.Configuration;
+import com.querydsl.sql.HSQLDBTemplates;
+import com.querydsl.sql.SQLQueryFactory;
+import com.querydsl.sql.SQLTemplates;
 
 @SpringBootApplication
 public class DemoApplication {
 
+    @Autowired
+    DataSource dataSource;
+
+    @Bean
+    Configuration configuration() {
+        SQLTemplates templates = new HSQLDBTemplates();
+        return new Configuration(templates);
+    }
+
+    @Bean
+    SQLQueryFactory sqlQueryFactory() {
+        return new SQLQueryFactory(configuration(), dataSource);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+        
     }
+
 }
